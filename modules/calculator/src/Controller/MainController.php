0c299b705e46fff81f571d9eeba77165cac64a9e
root <root@admins-student-5.nixsolutions.com>
2018-07-24 19:04:48 +0300

    v1

--- modules/calculator/src/Controller/MainController.php
+++ modules/calculator/src/Controller/MainController.php
@@ -2,23 +2,21 @@
 /**
  * @file
  * Contains \Drupal\calculator\Controller\Calculator.
- */
-
+**/
 
 namespace Drupal\calculator\Controller;
 
 
 use Drupal\Core\Controller\ControllerBase;
 
-/**
- * Объявляем наш класс-контроллер.
- */
-class CalculatorController extends ControllerBase {
+
+class MainController extends ControllerBase {
 
     /**
      * {@inheritdoc}
      */
-    public function calc() {
+
+    public function request() {
         $output = array();
 
         $output['#title'] = 'Calculator Page';
